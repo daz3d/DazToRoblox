fb2ff041e6576961ea141b784482c7bb13a0d7a0
danielbui78 <danielbui78@gmail.com>
2024-03-22 01:42:59 -0400

    Update merge_cage_att_from_template.py (attachment repositioning)

--- UgcDevKit/merge_cage_att_from_template.py
+++ UgcDevKit/merge_cage_att_from_template.py
@@ -2,8 +2,8 @@
 
 Will load the cage and attachment objects from a template file and merge them into the current scene.
 
-Requirements:
-- Blender 3.6 or later
+- Developed and tested with Blender 3.6.1 (Python 3.10.12)
+- Requires Blender 3.6 or later
 
 Usage:
     1. Open the .blend file where you want to merge the cage and attachment objects.
@@ -11,8 +11,12 @@ Usage:
 
 """
 
+REPOSITION_ATTACHMENTS = True
+REPOSITION_CAGES = True
+
 import os
 import bpy
+from mathutils import Matrix
 
 # Path to the .blend template file
 try:
@@ -84,6 +88,30 @@ def load_objects_from_blend(file_path, directory):
         data_to.objects = list(return_list)
     return list(return_list)
 
+def get_bone_world_matrix(bone, arm=None):
+    if arm is None:
+        arm = bone.id_data
+    arm_matrix = arm.matrix_world
+    world_matrix_bone = arm_matrix @ bone.matrix
+    return world_matrix_bone
+
+def calc_local_bone_offset(obj, bone):
+    if bone is None:
+        return None
+    arm = bone.id_data
+    arm_matrix = arm.matrix_world
+    world_matrix_bone = arm_matrix @ bone.matrix
+    loc,rot,scale = world_matrix_bone.decompose()
+    world_matrix_bone = Matrix.Translation(loc)
+    if obj is not None:
+        loc, rot, scale = obj.matrix_world.decompose()
+        world_matrix_obj = Matrix.Translation(loc)
+        local_matrix = world_matrix_bone.inverted() @ world_matrix_obj
+    else:
+        local_matrix = world_matrix_bone
+
+    return local_matrix
+
 def main():
     # Load the objects
     import_list = load_objects_from_blend(blend_file_path, blend_directory)
@@ -100,14 +128,30 @@ def main():
         else:
             obj.select_set(False)
 
-    # Assume the armature is already in the scene and named 'Armature'
+    # Assume the armature is already in the scene
     arm = bpy.data.armatures[0]
     arm_obj = bpy.data.objects[arm.name]
+    source_arm = bpy.data.armatures[1]
+    source_arm_obj = bpy.data.objects[source_arm.name]
+
+    source_obj_matrix = {}
+    source_bone_matrix = {}
+    source_bone_offset = {}
 
     for obj in loaded_objects:
         if obj.name not in lookup_table:
             continue
         world_matrix = obj.matrix_world.copy()
+        source_obj_matrix[obj.name] = world_matrix
+        # calc local bone offset
+        bone_name = obj.parent_bone
+        bone = source_arm_obj.pose.bones[bone_name]
+        bone_matrix = get_bone_world_matrix(bone, source_arm_obj)
+        source_bone_matrix[obj.name] = bone_matrix
+        local_matrix = calc_local_bone_offset(obj, bone)
+        # print("DEBUG: merge_cage_att_from_template.py: main(): local_matrix = \n", local_matrix)
+        source_bone_offset[obj.name] = local_matrix
+        # unparent from source armature
         obj.parent = None
         obj.matrix_world = world_matrix
     
@@ -116,40 +160,40 @@ def main():
         if obj.name not in lookup_table:
             continue
         world_matrix = obj.matrix_world.copy()
+        # reparent to destination armature
         obj.parent = arm_obj
         obj.matrix_world = world_matrix
-        # obj.matrix_parent_inverse = arm_obj.matrix_world.inverted()
 
     # Now, parent each object to the specific bone mentioned in the lookup table
     for obj in loaded_objects:
         if obj.name not in lookup_table:
             continue
         world_matrix = obj.matrix_world.copy()
+        # attachment world matrix
+        loc_obj,rot_obj,scale_obj = world_matrix.decompose()
         bone_name = lookup_table[obj.name]
         bone = arm_obj.pose.bones[bone_name]
+
+        # target bone location
+        world_bone_matrix = get_bone_world_matrix(bone)
+        loc_bone,rot_bone,scale_bone = world_bone_matrix.decompose()
+        # source offset location
+        local_bone_offset = source_bone_offset[obj.name]
+        loc_offset,rot_offset,scale_offset = local_bone_offset.decompose()
+
         obj.parent_bone = bone_name
         obj.parent_type = 'BONE'
-        obj.matrix_world = world_matrix
-        # obj.matrix_parent_inverse = arm_obj.matrix_world.inverted()
 
-    # remove missing or unused images
-    print("DEBUG: deleting missing or unused images...")
-    for image in bpy.data.images:
-        is_missing = False
-        if image.filepath:
-            imagePath = bpy.path.abspath(image.filepath)
-            if (not os.path.exists(imagePath)):
-                is_missing = True
+        # new attachment world matrix (target bone location + source offset location)
+        new_world_matrix = Matrix.Translation(loc_bone + loc_offset) @ rot_obj.to_matrix().to_4x4() @ Matrix.Diagonal(scale_obj).to_4x4()
 
-        is_unused = False
-        if image.users == 0:
-            is_unused = True
-
-        if is_missing or is_unused:
-            bpy.data.images.remove(image)
+        if REPOSITION_ATTACHMENTS:
+            obj.matrix_world = new_world_matrix
+        else:
+            obj.matrix_world = world_matrix
 
     # cleanup all unused and unlinked data blocks
-    print("DEBUG: main(): cleaning up unused data blocks...")
+    # print("DEBUG: main(): cleaning up unused data blocks...")
     bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)            
 
 if __name__ == "__main__":
