54ecd354af24610b18269da89045c93ab0409f8f
danielbui78 <danielbui78@gmail.com>
2024-04-29 16:04:29 -0400

    Updated bridge library

--- DazStudioPlugin/version.h
+++ DazStudioPlugin/version.h
@@ -4,7 +4,7 @@
 // Version number for DzGodotBridge
 #define PLUGIN_MAJOR	2024
 #define PLUGIN_MINOR	1
-#define PLUGIN_REV		1
-#define PLUGIN_BUILD	94
+#define PLUGIN_REV		2
+#define PLUGIN_BUILD	96
 
 #define PLUGIN_VERSION	DZ_MAKE_VERSION( PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_REV, PLUGIN_BUILD )
