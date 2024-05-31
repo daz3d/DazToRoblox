174de0e6f0da9a0a1b269e44fd8a03e7ec3b57e6
danielbui78 <danielbui78@gmail.com>
2024-05-31 10:20:43 -0400

    automate RC version updates

---
+++ DazStudioPlugin/real_version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define PRODUCT_VERSION_STRING	"2024 version 1.2"
+#define VER_MAJOR	2024
+#define VER_MINOR	1
+#define VER_REV		2
+#define VER_BUILD	133
+
+#define TOSTRING(x) #x
+#define VERSION_STRING	TOSTRING(VER_MAJOR) "." TOSTRING(VER_MINOR) "." TOSTRING(VER_REV) "." TOSTRING(VER_BUILD)
