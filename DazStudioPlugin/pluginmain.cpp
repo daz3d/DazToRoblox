9ff58c2b12e1f698dee3295ad6dec6187e72ea92
danielbui78 <danielbui78@gmail.com>
2024-06-05 10:30:20 -0400

    version update, bridge library update

--- DazStudioPlugin/pluginmain.cpp
+++ DazStudioPlugin/pluginmain.cpp
@@ -21,9 +21,10 @@ Documentation and source code are available on <a href = \"https://github.com/da
 DZ_PLUGIN_CLASS_GUID(DzRobloxAction, cffcd78e-3917-4b77-a60f-c559753226ee);
 NEW_PLUGIN_CUSTOM_CLASS_GUID(DzRobloxDialog, 0ab8fdb4-6575-4a7c-ad4b-456a687c1c81);
 
-//#define BETA_GUID c9346f33-f576-4003-813a-fb1e63798114
-#define BETA_GUID c9346f33-f576-4003-813a-fb1e63798115
-DZ_PLUGIN_ACTIVATION_POLICY(BETA_GUID, new DzTrialDaysActivationPolicy(60));
+//c9346f33-f576-4003-813a-fb1e63798114
+//c9346f33-f576-4003-813a-fb1e63798115
+//c9346f33-f576-4003-813a-fb1e63798116
+DZ_PLUGIN_ACTIVATION_POLICY(c9346f33-f576-4003-813a-fb1e63798116, new DzTrialDaysActivationPolicy(7));
 //DZ_PLUGIN_REGISTRATION_PREFIX(DZRBLX, "011", "Roblox Exporter");
 
 #ifdef UNITTEST_DZBRIDGE
